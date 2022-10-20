0006b36eaa8501a12499acb7a535bf2a6cfb0da3
David-Ynaz <davukr@outlook.com>
2022-10-20 11:13:03 +0400

    first commit

---
+++ Car/Program.cs
@@ -0,0 +1,31 @@
+﻿
+Console.WriteLine("Выберите тип покрытия:\n" +
+    "1 - Сухой асфальт\n" +
+    "2 - Мокрая дорога\n" +
+    "3 - Укатанный снег\n" +
+    "4 - Обледененная дорога\n");
+int choice = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine("Скорость км.ч:");
+int km = int.Parse(Console.ReadLine());
+double s;
+switch(choice)
+{
+    case 1:
+        s = (1*km^2)/(254*0.7);
+        Console.WriteLine(s*100);
+        break;
+    case 2:
+        s = (1*km^2)/(254*0.4);
+        Console.WriteLine(s*100);
+        break;
+    case 3:
+        s = (1*km^2)/(254*0.2);
+        Console.WriteLine(s*100);
+        break;
+    case 4:
+        s = (1*km^2)/(254*0.1);
+        Console.WriteLine(s * 100);
+        break;
+}
